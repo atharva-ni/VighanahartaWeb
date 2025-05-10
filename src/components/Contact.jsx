3152642fc2e10dc7b2de07f01b745aba703b390e
AtharvaNighot0030 <atharva.nighot30@gmail.com>
2025-05-10 19:45:09 +0530

    add vercel.json

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react';
 import { MapPin, Phone, Mail, Clock, Facebook, Twitter, Linkedin, Youtube } from "lucide-react"
 import { useState } from "react"
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     base: '/', // Ensure the base is correct for deployment
+//     build: {
+//     outDir: 'dist', // Default output directory
+//   }
+//   ],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss(),],
+  base: '/', // Ensure the base is correct for deployment
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+});
